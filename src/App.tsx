13655f0c74d2c68d409ae428cf74b828d6626a1e
AnshSolutions <anshcoder9@gmail.com>
2024-11-04 13:59:48 +0530

    Update Node.js version and simplify build script

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import {AppProvider} from 'providers/application';
-import {RouterProvider} from 'providers/routes';
+import { AppProvider } from 'providers/application';
+import { RouterProvider } from 'providers/routes';
+import { HashRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <AppProvider>
-      <RouterProvider />
+      <HashRouter>
+        <RouterProvider />
+      </HashRouter>
     </AppProvider>
   );
 }
 
 export default App;
+
